2c5061139acaafea121b5f3d4548e88a10b76657
Fernanda Sampaio <fernanda@MacBook-Air-de-Fernanda.local>
2020-01-08 17:19:04 -0300

    con los botones funcionando

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
@@ -7,10 +6,11 @@ class App extends React.Component {
     super(props);
     this.state = {
       songs: [
-        
+
       ]
     };
     this.player = null;
+    this.pos = null;
   }
 
 
@@ -47,34 +47,58 @@ class App extends React.Component {
       .catch(error => console.log(error));
   }
 
-  playsong(e,i){
+  playsong(e, i) {
     e.preventDefault();
-    this.player=document.querySelectorAll('audio')[i]
+    this.player = document.querySelectorAll('audio')[i];
+    this.pos = i;
   }
 
+
+  siguientesong(e) {
+    e.preventDefault();
+    this.player = document.querySelectorAll('audio')[this.pos+1];
+    this.pos+=1;
+  }
+
+  anteriorsong(e) {
+    e.preventDefault();
+    this.player = document.querySelectorAll('audio')[this.pos-1];
+    this.pos-=1;
+  }
+  
+
   render() {
-        return(
+    return (
       <>
-      <div className="list-group">
-        {
-          this.state.songs.length > 0 &&
-          this.state.songs.map((song, i) => {
-            return (
-              <a key={i} href="/#" onClick={(e) => this.playsong(e,i)} className="list-group-item list-group-item-action">
-              {song.name}
-                <audio ref={(t)=>this.player=t}>
-                  <source src={"https://assets.breatheco.de/apis/sound/" + song.url} type="audio/mp3"/>
-                </audio>
-              </a>
-            )
-          })
-        }
-        <span>
-          <button type="button" className="btn btn-dark">antes</button>
-          <button type="button" className="btn btn-dark" onClick={() => this.player.play()}><i className="far fa-play-circle"></i></button>
-          <button type="button" className="btn btn-dark">despues</button>
-        </span>
-      </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 justify-content-center">
+            <h1 className="text-center" >Click in your favorite music</h1>
+              <div className="list-group">
+                {
+                  this.state.songs.length > 0 &&
+                  this.state.songs.map((song, i) => {
+                    return (
+                      <a key={i} href="/#" onClick={(e) => this.playsong(e, i)} className="list-group-item list-group-item-action">
+                        {song.name}
+                        <audio ref={(t) => this.player = t}>
+                          <source src={"https://assets.breatheco.de/apis/sound/" + song.url} type="audio/mp3" />
+                        </audio>
+                      </a>
+                    )
+                  })
+                }
+                {this.pos}
+                <span>
+                  <button type="button" className="btn btn-dark"onClick={(e) => this.anteriorsong(e)}><i className="far fa-arrow-alt-circle-left"></i></button>
+                  <button type="button" className="btn btn-dark" onClick={() => this.player.play()}><i className="far fa-play-circle"></i></button>
+                  <button type="button" className="btn btn-dark" onClick={() => this.player.pause()}><i className="far fa-pause-circle"></i></button>
+                  <button type="button" className="btn btn-dark"onClick={(e) => this.siguientesong(e)}><i className="far fa-arrow-alt-circle-left"></i></button>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
